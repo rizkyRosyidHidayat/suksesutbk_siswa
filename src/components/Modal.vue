11a4ce803d0603882ceeadd3ef43e9b599262e19
Rizky Rosyid Hidayat <rizky.hidayat@students.amikom.ac.id>
2020-10-05 09:34:13 +0700

    add scroll in modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -18,6 +18,6 @@ export default {
   background-color: rgba(0, 0, 0, 0.5);
   top: 0px;
   left: 0px;
-  @apply h-screen w-screen fixed z-20 pt-12;
+  @apply h-screen w-screen fixed z-20 py-12 overflow-y-auto;
 }
 </style>
